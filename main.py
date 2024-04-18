80c81f646f2d19dcfbf70f1442d8c7c65359be12
Deggi0 <93735832+Deggi0@users.noreply.github.com>
2024-04-18 18:11:46 +0200

    sistemazione footer e responsività + inizio layout login/register + aggiunti links a menu offcanvas + predisposti elementi x custom cookies

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def get_person(id):
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("registration/login.html")
     elif request.method == "POST":
         #check if credential are correct
         username = request.form["username"]
@@ -92,7 +92,7 @@ def login():
             session["username"] = username
             return redirect(url_for("user"))
         else:
-            return render_template("login.html")
+            return render_template("registration/login.html")
 
 @app.route('/user/')
 def user():
