b2cf40a9984a38fcb26620a392363417f3454280
le0o5 <123751394+le005@users.noreply.github.com>
2024-04-19 10:18:51 +0200

    sistemazione codice

--- main.py
+++ main.py
@@ -59,19 +59,19 @@ async def control(update: Update, context):
     val=liv_idro[-1]["VM"]
     liv=float(val)
     if(liv>=2.3):
-        response="AO ZI ER BRENTA STA AL PRIMO LIVELLO STA AD ALTEZZA",val
+        response="ALTEZZA ",val
         response = ' '.join(response)
         await invia_notifica(response)
     elif(liv>=2.8):
-        response="AO ZI ER BRENTA STA AL PRIMO LIVELLO STA AD ALTEZZA",val
+        response="ALTEZZA ",val
         response = ' '.join(response)
         await invia_notifica(response)
     elif(liv>=3.2):
-        response="AO ZI ER BRENTA STAMO A GIOCA A CARTE CON I PESCI TIE BECCATE STA ALTEZZA",val
+        response="ALTEZZA ",val
         response = ' '.join(response)
         await invia_notifica(response)
     else:
-        response="STIAMO NER BING CHILLING STIAMO AD ALTEZZA",val
+        response="ALTEZZA ",val
         response = ' '.join(response)
         await invia_notifica(response)   
         
