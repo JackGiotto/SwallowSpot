ef91209a6b4b1d7b8400269bf5c6f3442eca0ddd
Davidem298 <123751981+Davidem298@users.noreply.github.com>
2024-04-20 15:12:56 +0200

    Completed #4

--- query.sql
+++ query.sql
@@ -2,6 +2,13 @@
 create table topology       -- assegnazione delle città alle rispettive zone di rischio
 (
     city varchar(35) primary key not null,          -- nome della città
-    area varchar(5) not null                        -- sigla della zona
+    ID_area int not null,
+    constraint FK1_topology foreign key (ID_area) references area(ID_area) on delete cascade 
+)
+
+create table area
+(
+    ID_area int primary key not null auto_increment,
+    area_name varchar(5)
 )
 
