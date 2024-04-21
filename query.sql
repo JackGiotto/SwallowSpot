fbdfaff6b63f2a0f6a50f74cbabda363e370f687
Davidem298 <123751981+Davidem298@users.noreply.github.com>
2024-04-21 21:49:29 +0200

    #17

--- query.sql
+++ query.sql
@@ -1,14 +1,32 @@
 
-create table topology       -- assegnazione delle città alle rispettive zone di rischio
-(
-    city varchar(35) primary key not null,          -- nome della città
-    ID_area int not null,
-    constraint FK1_topology foreign key (ID_area) references area(ID_area) on delete cascade 
-)
+-- Query di creazione del DataBase SwallowSpot
 
-create table area
+create table Area       -- zona di allerta per ogni rischio
 (
-    ID_area int primary key not null auto_increment,
-    area_name varchar(5)
-)
+    ID_area int auto_increment primary key,     -- ID univoco per ogni singola zona
+    area_name varchar(7)                        -- nome della zona
+);
+
+create table Risk       -- rischio specifico
+(
+    ID_risk int auto_increment primary key,     -- ID univoco per ogni rischio
+    risk_name varchar(35)                       -- nome del tipo di rischio
+);
+
+create table Role       -- ruolo di ogni utente
+(
+    ID_role int auto_increment primary key,     -- ID univoco per ogni ruolo         
+    role_name varchar(35)                       -- nome di ogni ruolo
+);
+
+create table User       -- utente e i suoi dati
+(
+    ID_user int auto_increment primary key,         -- ID univoco per ogni utente
+    username varchar(35),                           -- username usato dall'utente per registrarsi al sito
+    password varchar(128),                          -- hash della password usata dall'utente per registrarsi al sito
+    ID_area int,                                    -- area in cui vive
+    ID_role int,                                    -- ruolo dell'utente nel sito
+    foreign key (ID_area) references Area(ID_area),
+    foreign key (ID_role) references Role(ID_role)
+);
 
