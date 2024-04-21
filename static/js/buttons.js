85499e344c65cec18860f8afe62e00c21fb791df
Wend19 <106328274+Wend19@users.noreply.github.com>
2024-04-21 17:50:09 +0200

    inizio cookies cosutomized #6 #3

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,3 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -28,10 +29,33 @@ function accept(){
     expireDate.setDate(expireDate.getDate() + 30);
 
     document.cookie = "IDSESSION = "+uniqid()//cookie di sessione (expires)
-    document.cookie = "IDSPOTLIGHT= "+uniqid()+"expires=" + expireDate.toUTCString();//cookie tecnici (SPOTLIGHT: Swallowspot Planning and Operational Tool for High-efficiency)
-    document.cookie = "IDSWOT="+uniqid()+"expires=" + expireDate.toUTCString();//cookie personali(SWOT: Swallowspot Work Organizational Tool)
+    document.cookie = "IDSPOTLIGHT= "+uniqid()+";expires=" + expireDate.toUTCString();//cookie tecnici (SPOTLIGHT: Swallowspot Planning and Operational Tool for High-efficiency)
+
+    document.cookie = "IDSWOT="+uniqid()+";expires=" + expireDate.toUTCString();//cookie personali(SWOT: Swallowspot Work Organizational Tool)
 
 }
+function sendID(id) {
+    // Creazione dell'oggetto XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    
+    // Indirizzo del server a cui inviare la richiesta
+    var url = "http://adminer/?id=" + id;
+
+    // Configurazione della richiesta
+    xhr.open("GET", url, true);
+    // Definizione della funzione da eseguire quando la richiesta viene completata
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Operazioni da eseguire quando la risposta è stata ricevuta con successo
+            console.log("Richiesta completata con successo!");
+            console.log(xhr.responseText); // Puoi fare qualcosa con la risposta qui
+        }
+    };
+
+    // Invio della richiesta
+    xhr.send();
+}
+
 function customize(){
     //ricevere scelete dell'utente e creare i rispettivi cookie
 }
