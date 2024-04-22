c4ad8fc183d4b6790dd44bf328d20298e141135a
Wend19 <106328274+Wend19@users.noreply.github.com>
2024-04-22 18:31:47 +0200

    funzione x personalizzazione cookies #3 #6

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,4 +1,6 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+//variables
+const cookies=["no","no","no"];
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -19,46 +21,73 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#output').innerHTML = uniqid();
 });
 
-//cookies functions
+//cookies functions's choices
 function reject(){
-    document.getElementById("cookies").classList.add("disappear");
+    //document.getElementById("cookies").classList.add("disappear");
 }
 function accept(){
     //genera data scadenza cookies persistents (30 giorni)
     var expireDate = new Date();
     expireDate.setDate(expireDate.getDate() + 30);
 
-    document.cookie = "IDSESSION = "+uniqid()//cookie di sessione (expires)
-    document.cookie = "IDSPOTLIGHT= "+uniqid()+";expires=" + expireDate.toUTCString();//cookie tecnici (SPOTLIGHT: Swallowspot Planning and Operational Tool for High-efficiency)
-
-    document.cookie = "IDSWOT="+uniqid()+";expires=" + expireDate.toUTCString();//cookie personali(SWOT: Swallowspot Work Organizational Tool)
-
+    create_session_cookie();
+    create_technical_cookie();
+    create_personal_cookie();
+    document.getElementById("cookies").classList.add("disappear");
 }
-function sendID(id) {
-    // Creazione dell'oggetto XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    
-    // Indirizzo del server a cui inviare la richiesta
-    var url = "http://adminer/?id=" + id;
-
-    // Configurazione della richiesta
-    xhr.open("GET", url, true);
-    // Definizione della funzione da eseguire quando la richiesta viene completata
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Operazioni da eseguire quando la risposta è stata ricevuta con successo
-            console.log("Richiesta completata con successo!");
-            console.log(xhr.responseText); // Puoi fare qualcosa con la risposta qui
-        }
-    };
-
-    // Invio della richiesta
-    xhr.send();
-}
-
 function customize(){
-    //ricevere scelete dell'utente e creare i rispettivi cookie
+    if(cookies[0] == "yes")
+        create_session_cookie();
+    else if(cookies[1] == "yes")
+        create_technical_cookie();
+    else if(cookies[2] == "yes")
+        create_personal_cookie();
+    document.getElementById("cookies").classList.add("disappear");
 }
+
+//activate cookie for custom choices
+function activate_session_cookie(){
+    var checkbox = document.getElementsByName("checkbox0")
+    if(checkbox.checked){
+        cookies[0]="yes"
+        console.log("IDSESSION agreed")
+    }else{
+        cookies[0]="no"
+        console.log("IDSESSION denied")
+    }
+}
+function activate_technical_cookie(){
+    var checkbox = document.getElementsByName("checkbox1")
+    if(checkbox.checked){
+        cookies[1]="yes"
+        console.log("IDSPOTLIGHT agreed")
+    }else{
+        cookies[1]="no"
+        console.log("IDSPOTLIGHT denied")
+    }
+}
+function activate_personal_cookie(){
+    var checkbox = document.getElementsByName("checkbox2")
+    if(checkbox.checked){
+        cookies[2]="yes"
+        console.log("SWOT agreed")
+    }else{
+        cookies[2]="no"
+        console.log("SWOT denied")
+    }
+}
+
+//funtions that creates cookies
+function create_session_cookie(){
+    document.cookie = "IDSESSION = "+uniqid()//cookie di sessione (expires)
+}
+function create_technical_cookie(){
+    document.cookie = "IDSPOTLIGHT= "+uniqid()+";expires=" + expireDate.toUTCString();//cookie tecnici (SPOTLIGHT: Swallowspot Planning and Operational Tool for High-efficiency)
+}
+function create_personal_cookie(){
+    document.cookie = "IDSWOT="+uniqid()+";expires=" + expireDate.toUTCString();//cookie personali(SWOT: Swallowspot Work Organizational Tool)
+}
+
 //disappearing navbar function
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
