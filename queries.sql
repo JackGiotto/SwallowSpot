e8149a70c094ae92b5bac2ed88c2928007400de5
Davidem298 <123751981+Davidem298@users.noreply.github.com>
2024-04-29 18:21:14 +0200

    Added queries for #20

---
+++ queries.sql
@@ -0,0 +1,18 @@
+-- visualizzare nomi delle città e relativa zona
+select city_name, area_name
+from Topology
+join Area on Topology.ID_area = Area.ID_area 
+
+-- selezionare utente e password per login
+select username, password
+from User
+where username = 'variabile_username' and password = 'variabile_password';
+
+-- Dovresti farmi una query che possa ottenere il livello di criticità di VENE B per ogni tipologia di pericolo, una per il primo bollettino e la stessa query per le nevicate 
+-- dell'ultimo bollettino
+select *
+from Area
+join Criticalness on Area.ID_area = Criticalness.ID_area
+join Color on Criticalness.ID_color = Color.ID_color
+join Risk on Criticalness.ID_risk = Risk.ID_risk
+where area_name = 'variabile_area';
--- query.sql
+++
@@ -1,4 +0,0 @@
--- visualizzare nomi delle città e relativa zona
-select city_name, area_name
-from Topology
-join Area on Topology.ID_area = Area.ID_area 
