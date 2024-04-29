96bede8709d84a5f9a13a75e69af3a2c416098f5
Wend19 <106328274+Wend19@users.noreply.github.com>
2024-04-29 19:06:34 +0200

    inserimento lavoro login e signup di s02675 (stefani) in templates #11 #20

--- views/auth/auth.py
+++ views/auth/auth.py
@@ -1,17 +1,75 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
+import pymysql.cursors
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
+def executeQuery(query):
+    connection = pymysql.connect(host = "localhost",
+                                user = "s02675",
+                                password = "Ohmah6",
+                                database = "s02675",
+                                cursorclass = pymysql.cursors.DictCursor
+                                )
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            res = cursor.fetchall()
+            connection.commit()
+            return res
+
 @auth_bp.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == "GET":
+    if request.method == "GET":#accesso risorsa
         return render_template("auth/login.html")
-    elif request.method == "POST":
-        pass
+    elif request.method == "POST":#login
+        username = request.form["username"]
+        password = request.form["password"]
+        #confronto credenziali con DB
+        sql = "CREATE VIEW UserView AS SELECT username,password,CASE WHEN username = '"+username+"' AND password = '"+password+"' THEN 'true' ELSE 'false' END AS user_exists FROM User;"
+        executeQuery(sql)
+        result = executeQuery("SELECT user_exists FROM UserView;")#restituisce lista-dizionario
+        executeQuery("DROP VIEW UserView;")
+        #confronto e reindirizzamento
+        if(result[0]["user_exists"] == "true"):
+            #stabilimento sessione
+            session.permanent = True
+            session["username"] = username
+            return render_template("home.html")
+        else:
+            return render_template("auth/login.html", msg="credenziali non corrette")
+    else:
+        return render_template("auth/login.html")
 
 @auth_bp.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if request.method == "GET":
         return render_template("auth/signup.html")
     elif request.method == "POST":
-        pass
+        #check if credential are correct
+        username = request.form["username"]
+        password = request.form["password"]
+        password2 =request.form["password2"]
+        if(password != password2):
+            return render_template("auth/signup.html", msg= "credeziali non corrette")
+        elif(password == password2):
+            #confronto credenziali con DB
+            sql = "CREATE VIEW UserView AS SELECT username,password,CASE WHEN username = '"+username+"' THEN 'true' ELSE 'false' END AS user_exists FROM User;"
+            executeQuery(sql)
+            result = executeQuery("SELECT user_exists FROM UserView;")#restituisce lista-dizionario
+            executeQuery("DROP VIEW UserView;")
+            #confronto e reindirizzamento
+            if(result[0]["user_exists"] == "true"):#true -> utente gia presente nel db 
+                return render_template("auth/signup.html", msg="username non disponibile")
+            elif(result[0]["user_exists"] == "false"):#false -> utente non presente nel db
+                #registrazione credenziali utente nel db 
+
+                #MANCA ZONA DI RESIDENZA !!!!!
+                executeQuery("INSERT INTO `User` (`username`, `password`, `ID_area`, `ID_role`)VALUES ('"+username+"', '"+password+"','5', '1');")
+                
+                #stabilimento sessione
+                session.permanent = True
+                session["username"] = username
+                return redirect(url_for("user"))
+        else:
+            return render_template("auth/signup.html")
+
