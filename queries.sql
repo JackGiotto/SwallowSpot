274318c540494f80eb5208655afd1313d13360fb
Davidem298 <123751981+Davidem298@users.noreply.github.com>
2024-04-30 11:35:58 +0200

    Completed #60

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ select username, password
 from User
 where username = 'variabile_username' and password = 'variabile_password';
 
--- Dovresti farmi una query che possa ottenere il livello di criticità di VENE B per ogni tipologia di pericolo, una per il primo bollettino e la stessa query per le nevicate 
+-- #20 Dovresti farmi una query che possa ottenere il livello di criticità di VENE B per ogni tipologia di pericolo, una per il primo bollettino e la stessa query per le nevicate 
 -- dell'ultimo bollettino
 select *
 from Area
@@ -16,3 +16,7 @@ join Criticalness on Area.ID_area = Criticalness.ID_area
 join Color on Criticalness.ID_color = Color.ID_color
 join Risk on Criticalness.ID_risk = Risk.ID_risk
 where area_name = 'variabile_area';
+
+-- #60 You have to returning the list (by default dict json) by querys on our db so s02675 (stefani) can properly create the menu for the users during their sign-up
+select city_name
+from Topology;
