615e53331fcf45a0ad39bb76b310ec8f41f1f907
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-04-30 11:34:26 +0200

    started template rendeing #31

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ app.register_blueprint(profile_bp)
 app.register_blueprint(reports_bp, url_prefix='/{}'.format(reports_bp.name))
 
 if __name__ == "__main__":
-    app.run(debug = True, host="0.0.0.0", port=11599)
+    app.run(debug = True, host="0.0.0.0", port=5000)
--- views/home/home.py
+++ views/home/home.py
@@ -1,7 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 home_bp = Blueprint('home', __name__, template_folder='templates')
 
 @home_bp.route('/')
 def home():
-    return render_template("home.html")
+    if "username" in session:
+        return render_template("home.html")
+    else:
+        region_name = "Vene-A"
+        hydraulic = "green"
+        hydro_geo = "red"
+        storms = "orange"
+        bulletin_data = f'''<div class="risk-out" id="vene-a">
+                                <h2>{region_name}</h2>
+                                <div class="risk-in">
+                                    <div class="risk">
+                                        <span class="circle {hydraulic}"></span>
+                                        <p>Rischio idraulico</p>
+                                    </div>
+                                    <div class="risk">
+                                        <span class="circle {hydro_geo}"></span>
+                                        <p>Rischio idro-geologico</p>
+                                    </div>
+                                    <div class="risk">
+                                        <span class="circle {storms}"></span>
+                                        <p>Rischio idro-geologico per temporale</p>
+                                    </div>
+                                </div>
+                                <p class="basin">Bacino correlato: Alto Piave</p>
+                                <p class="last-update" id="update-vene-a">Ultimo aggiornamento: <heregoesthedate /></p>
+                             </div>'''
+        # query to get user data
+        # query to get the last bullettin of an area
+        return render_template("home.html", bulletin_data = bulletin_data)
