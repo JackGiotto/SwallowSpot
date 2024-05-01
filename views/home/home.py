925225ad28c57a1055fcbb896727aa329849afb6
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-05-01 21:54:43 +0200

    added db query for user's area #31

--- views/home/home.py
+++ views/home/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session
+from models import db
 
 home_bp = Blueprint('home', __name__, template_folder='templates')
 
@@ -7,22 +8,28 @@ def home():
     print (session)
     if "username" not in session:
         bulletin_data = '''
-                            Per rimanere aggiornato sulla tua zona esegui l'accesso
-                            <a href="/auth/login/" class="btn btn-success m-2 btn-lg"> Accedi</a>'''
+                    Per rimanere aggiornato sulla tua zona esegui l'accesso
+                    <a href="/auth/login/" class="btn btn-success m-2 btn-lg"> Accedi</a>'''
     else:
         # debug
-        region_name = "Vene-A"
+        #area_name = "Vene-A"
         hydraulic = "green"
         hydro_geo = "red"
         storms = "orange"
         update = "13-04-2024"
 
-        # query to get user data
+        # query to get user area
+        query = f"""SELECT Area.area_name
+                    FROM User
+                    JOIN Area ON User.ID_area = Area.ID_area
+                    WHERE User.username = '{session["username"]}';"""
+        
         # query to get the last bullettin of the user area
 
-        print ("ciao ciao")
+
+        area_name = db.executeQuery(query)[0]["area_name"]
         bulletin_data = f'''<div class="risk-out" id="vene-a">
-                                <h2>{region_name}</h2>
+                                <h2>{area_name}</h2>
                                 <div class="risk-in">
                                     <div class="risk">
                                         <span class="circle {hydraulic}"></span>
