5f1981ceba89d6d515867d3742e2224725335f4a
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-05-01 22:14:44 +0200

    added logout button

--- views/profile/profile.py
+++ views/profile/profile.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, redirect, session
+from flask import Blueprint, render_template, redirect, session, url_for
+import json
 
 profile_bp = Blueprint('profile', __name__, template_folder='templates')
 
@@ -8,3 +9,8 @@ def user():
         return render_template("user/profile.html")
     else:
         return redirect("/auth/login")
+
+@profile_bp.route('/profile/logout/', methods=['POST'])
+def logout():
+    session.clear()
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
