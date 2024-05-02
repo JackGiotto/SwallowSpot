0d2a204c9a1bfb31ae37af686dade998edd18517
Wend19 <106328274+Wend19@users.noreply.github.com>
2024-05-02 22:16:03 +0200

    modifiche piccole ai form e creazione funzioni backend (py) per aggiornare i dati nel db x username, zona di residenza

--- views/profile/profile.py
+++ views/profile/profile.py
@@ -7,21 +7,42 @@ profile_bp = Blueprint('profile', __name__, template_folder='templates')
 def user():
     cities_query = '''SELECT city_name, ID_city FROM Topology;'''
     cities = db.executeQuery(cities_query)
-           
+
     if "username" in session:
+        id_user = db.executeQuery("SELECT ID_user FROM User WHERE username ='"+session["username"]+"';")
+        id_user = id_user[0]["ID_user"]
         if request.method == "GET":
             return render_template("user/profile.html", cities=cities, username=session["username"])
 
         elif request.method == "POST":
-            city = request.form["city"]
-            ID_area_query = f''' SELECT Topology.ID_area 
-                                    FROM Topology
-                                    WHERE ID_city = {city}
-                                '''
-            ID_area = db.executeQuery(ID_area_query)
-            #uploads into database
-            nusername= request.form[""]
+            if "new_username" in request.form:
+                new_username = request.form["new_username"]
+                db.executeQuery("UPDATE User SET username = '"+str(new_username)+"' WHERE ID_user = '"+str(id_user)+"';")
+                session["username"]=new_username
+                return render_template("user/profile.html", cities=cities, username=session["username"])
+
+            elif "city" in request.form:
+                new_zone = request.form["city"]#a quanto pare viene restituito l'id ma non ho voglia di capire perché... buonanotte... oh no buongiorno tini
+                update= "UPDATE User SET ID_area = '"+str(new_zone)+"' WHERE ID_user = "+str(id_user)+";"
+                db.executeQuery(update)
+
+            elif "psw_change" in request.form:#questo lo faccio domani chill
+                new_password = request.form["new_password"]
 
             return render_template("user/profile.html")
+        elif request.method== "DELETE":
+            pass
     else:
         return redirect("/auth/login")
+
+
+
+
+        #city = request.form["city"]
+            #ID_area_query = f''' SELECT Topology.ID_area 
+            #                        FROM Topology
+             #                       WHERE ID_city = {city}
+            #                    '''
+            #ID_area = db.executeQuery(ID_area_query)
+            #uploads into database
+            
