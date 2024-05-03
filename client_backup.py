60a86c5e3acf331d3238d85085298cee289791c0
Davidem298 <123751981+Davidem298@users.noreply.github.com>
2024-05-03 10:21:11 +0200

    connection with SSL but without encryption

--- client_backup.py
+++ client_backup.py
@@ -134,6 +134,7 @@ except Exception as e:
 
 # mysqldump -u martini -p "Swallow Spot" > backup.sql
 
+
 import socket
 import ssl
 
@@ -146,7 +147,7 @@ context.load_verify_locations("certificate/server.crt")
 
 # Connect to the server
 ssl_client_socket = context.wrap_socket(client_socket, server_hostname="localhost")
-ssl_client_socket.connect(('192.168.0.113', 8080))
+ssl_client_socket.connect(('127.0.0.1', 8080))
 
 # Send data
 ssl_client_socket.send("Hello from client!".encode())
@@ -158,3 +159,21 @@ print("Response from server:", response.decode())
 # Close the connection
 ssl_client_socket.close()
 
+
+"""
+import ssl
+import socket
+
+if __name__ == "__main__":
+    # Create a context, just like as for the server
+    context = ssl.create_default_context()
+    # Load the server's CA
+    context.load_verify_locations("certificate/server.crt")
+    
+    # Wrap the socket, just as like in the server.
+    conn = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname="localhost")
+    
+    # Connect and send data! Standard python socket stuff can go here.
+    conn.connect(("localhost", 8080))
+    conn.sendall(b"Hello, server! This was encrypted.")
+"""
--- server_backup.py
+++ server_backup.py
@@ -1,5 +1,37 @@
+"""
+import ssl
+import socket
 
+BUFF_SIZE = 1024
+
+if __name__ == "__main__":
+    # First, create a context. The default settings are probably the best here. 
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    # Load the CA (self-signed, in this case) and the corresponding private key (also self-signed, in this case)
+    context.load_cert_chain(certfile="certificate/server.crt", keyfile="certificate/server.key")
+
+    # Create a standard TCP socket, bind it to an address, and listen for connections
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("0.0.0.0", 8080))
+    s.listen()
+
+    # Start accepting incoming connections
+    accepting = True 
+    try:
+        while accepting:
+            # conn is a standard python socket, addr is where it originated
+            conn, addr = s.accept()
+            # wrap the standard socket with the SSLContext, now it is a secure connection
+            with context.wrap_socket(conn, server_side=True) as secure_conn:
+                # data can be read/sent just like as in standard sockets
+                data = secure_conn.recv(BUFF_SIZE)
+                print(data)
+    except socket.timeout:
+        pass
+"""
+    
 # process running in the backup server for Swallow Spot DB
+
 
 import socket
 import ssl
@@ -34,5 +66,6 @@ while True:
     
     # Close the connection
     ssl_client_socket.close()
+
 
 # per creare certificato: openssl req -newkey rsa:2048 -nodes -keyout server.key -x509 -days 365 -out server.pem
