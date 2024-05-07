c2b59555c73a3dfe7eb525009cd565042451c498
Deggi0 <93735832+Deggi0@users.noreply.github.com>
2024-05-07 18:28:19 +0200

    fix darkmode + eliminazione account + sistemazione inserimento telegram id

--- static/js/profile_actions.js
+++ static/js/profile_actions.js
@@ -28,13 +28,15 @@ function checkInput(input) {
     html_block = "";
     // Verifica se l'input contiene solo numeri e ha una lunghezza compresa tra 1 e 9
     if (/^\d{9}$/.test(tgid)) {
-        errorMsg.innerHTML = "ID valido";
-        errorMsg.style.color = "green";
+        errorMsg.innerHTML = "";
         html_block = '<i class="fa-solid fa-check"></i>';
         confirm.disabled = false;
+    } else if (tgid == '') {
+        errorMsg.innerHTML = "";
+        html_block = '<i class="fa-solid fa-xmark"></i>';
+        confirm.disabled = true;
     } else {
         errorMsg.innerHTML = "ID non valido: deve contenere 9 numeri";
-        errorMsg.style.color = "darkred";
         html_block = '<i class="fa-solid fa-xmark"></i>';
         confirm.disabled = true;
     }
