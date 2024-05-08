892aec4d284fe4d091d88f2c0b805a8c7176f395
le0o5 <123751394+le005@users.noreply.github.com>
2024-05-08 10:24:52 +0200

    sistemazione codice per conessione al database

--- views/profile/profile.py
+++ views/profile/profile.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, session, url_for
+from flask import Blueprint, render_template, redirect, session, url_for,request
 import json
+import pymysql
+import os
 
 profile_bp = Blueprint('profile', __name__, template_folder='templates')
 
@@ -19,18 +21,29 @@ def logout():
 def admin():
     return render_template("user/admin_profile.html")
 
-@app.route('/insert_chat_id', methods=['POST'])
+@profile_bp.route('/profile/insert_chat_id', methods=['GET', 'POST'])
 def insert_chat_id():
-    try:
+        print("cd")
+        db = pymysql.connect (
+            host= os.getenv("SERVERNAME"),
+            user= os.getenv("DBUSER"),
+            password= os.getenv("PASSWORD"),
+            database= os.getenv("DBNAME"),
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        # Creare un cursore per eseguire le query
+        cursor = db.cursor()
+        print("cd")
+        tag = request.form.get['chat_id']
+        print("cd")
+        # Get the username/password associated with this tag
+        chat_id = tag_lookup(tag)
+        print("cd")
         # Recupera i dati inviati dal client
-        chat_id = request.json['chat_id']
-        username = request.json['username']
-
+        username = session["username"]
+        print("cd")
         # Esegue la query per aggiornare l'ID di chat nel database
-        query = "UPDATE tabella SET ID_telegram = %s WHERE username = %s"
+        query = "UPDATE Admin SET ID_telegram = %s WHERE username = %s"% (chat_id, username) 
+        print(query)
         cursor.execute(query, (chat_id, username))
         db.commit()
-
-        return jsonify({"success": True, "message": "Chat ID inserito con successo"})
-    except Exception as e:
-        return jsonify({"success": False, "error": str(e)})
