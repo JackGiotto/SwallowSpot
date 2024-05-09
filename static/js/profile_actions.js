bd6e07a2e19336cce5b30eef81773bf2137f52b0
le0o5 <123751394+le005@users.noreply.github.com>
2024-05-09 21:18:00 +0200

    modifica aggiunta id group

--- static/js/profile_actions.js
+++ static/js/profile_actions.js
@@ -19,75 +19,31 @@ function logout() {
 }
 
 //on admin page
+function checkInputs() {
+    var tgid = document.getElementById("ChatID").value.trim();
+    var gtgid = document.getElementById("GroupID").value.trim();
+    var errorMsg1 = document.getElementById("error");
+    var errorMsg2 = document.getElementById("error2");
+    var confirmButton = document.getElementById("confirmButton");
 
-function checkInput(input) {
-    var tgid = input.value.trim();
-    var errorMsg = document.getElementById("error");
-    var confirm = document.getElementById("confirmTgID");
-    $("#confirmIDcheck").empty();
-    html_block = "";
-    // Verifica se l'input contiene solo numeri e ha una lunghezza compresa tra 1 e 9
-    if (/^\d{9}$/.test(tgid)) {
-        errorMsg.innerHTML = "";
+    // Verifica se l'input ChatID è valido
+    var validChatID = /^\d{9}$/.test(tgid);
+    if (validChatID) {
+        errorMsg1.innerHTML = "";
         html_block = '<i class="fa-solid fa-check"></i>';
-        confirm.disabled = false;
-    } else if (tgid == '') {
-        errorMsg.innerHTML = "";
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-        confirm.disabled = true;
     } else {
-        errorMsg.innerHTML = "ID non valido: deve contenere 9 numeri";
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-        confirm.disabled = true;
+        errorMsg1.innerHTML = "ID non valido: deve contenere 9 numeri";
     }
-    $("#confirmIDcheck").append(html_block)
-}
 
-function checkGroupInput(input) {
-    var tgid = input.value.trim();
-    var errorMsg = document.getElementById("error2");
-    var confirm = document.getElementById("confirmTgGroupID");
-    $("#confirmGroupIDcheck").empty();
-    html_block = "";
-    // Verifica se l'input contiene solo numeri e ha una lunghezza compresa tra 1 e 9
-    if (/^\d{14}$/.test(tgid)) {
-        errorMsg.innerHTML = "";
+    // Verifica se l'input GroupID è valido
+    var validGroupID = /^\d{13}$/.test(gtgid);
+    if (validGroupID) {
+        errorMsg2.innerHTML = "";
         html_block = '<i class="fa-solid fa-check"></i>';
-        confirm.disabled = false;
-    } else if (tgid == '') {
-        errorMsg.innerHTML = "";
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-        confirm.disabled = true;
     } else {
-        errorMsg.innerHTML = "ID non valido: deve contenere 14 numeri";
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-        confirm.disabled = true;
+        errorMsg2.innerHTML = "ID non valido: deve contenere 14 numeri";
     }
-    $("#confirmGroupIDcheck").append(html_block)
-}
 
-//check if IP is right
-function checkInputIP(input) {
-    var IPaddress = input.value.trim();
-    var doBackup = document.getElementById("backupBtn");
-    var error = document.getElementById("IPerror");
-
-    $("#confirmIPcheck").empty();
-    html_block = "";
-
-    // Check if the input is a valid IP address
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(IPaddress)) {
-        error.innerHTML = "";
-        doBackup.disabled = false; // Enable doBackup
-        html_block = '<i class="fa-solid fa-check"></i>';
-    } else if (IPaddress == '') {
-        error.innerHTML = "";
-        doBackup.disabled = true; // Disable doBackup
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-    } else {
-        error.innerHTML = "L'indirizzo IP inserito non &egrave valido";
-        doBackup.disabled = true; // Disable doBackup
-        html_block = '<i class="fa-solid fa-xmark"></i>';
-    }
-    $("#confirmIPcheck").append(html_block)
+    // Abilita il pulsante di conferma solo se entrambi gli input sono validi
+    confirmButton.disabled = !(validChatID && validGroupID);
 }
