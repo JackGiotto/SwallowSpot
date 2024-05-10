afb19ddd86e08ce78559f717311832aaa95cff3c
Wend19 <106328274+Wend19@users.noreply.github.com>
2024-05-10 10:52:19 +0200

    completamento reciver mail

--- mail/mail_rx.py
+++ mail/mail_rx.py
@@ -1,14 +1,65 @@
 import imaplib
-import emails
+import email
 import webbrowser
 import os
-from email_header import decode_header
 
 #credentials
-email = "swallowspottesting@gmail.com"
-password = "RondineSpottatrice"
-imap_server = ""
+MAIL = "swallowspottesting@gmail.com"
+__PASSWORD = "RondineSpottatrice"
+IMAP_SERVER = "imap.gmail.com"
 
-#connections
-imap.login
+mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+
+def emials_fetch():
+     #select the mail's field where mails arrives
+     mail.select("inbox")
+
+     #indexes all the msgs in the inbox
+     status, data = mail.search(None, 'ALL')
+     mail_ids = data[0]
+
+     #list the msgs indexed
+     id_list= mail_ids.split()
+
+     #for every id in mailbox fetch the relative data 
+     for num in id_list:
+          status, data = mail.fetch(num, '(RFC822)')#std who define the format of mails for fetching to IMAP server
+          raw_email = data[0][1]
+     
+     #conversion form byte inconsistent data to email obj
+     msg = email.message_from_bytes(raw_email)
+     
+     #extraction of trasmittion informations form obj (msg)
+     sender = msg['From']
+     subject = msg['Subject']
+     body = None
+     
+     if msg.is_multipart():
+          for part in msg.walk():
+               content_type = part.get_content_type()
+               content_disposition = str(part.get("Content-Disposition"))
+               try:
+                    body = part.get_payload(decode=True).decode()
+               except:
+                    pass
+     else:
+          body = msg.get_payload(decode=True).decode()
+     
+     print('From:', sender)
+     print('Subject:', subject)
+     print('Body:', body)
+     print('-' * 50)
+    
+def main():
+     #connection to imap server
+     print("pronto per connetterti")
+     mail.login(MAIL, __PASSWORD)
+     print("ok connesso")
+     emials_fetch()
+
+
+if __name__ == '__main__':
+    main()
+else:    # close connection
+     mail.logout()
 
