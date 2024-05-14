ec6d71fcc4be2da505e25074ba78dbd67885d180
Deggi0 <93735832+Deggi0@users.noreply.github.com>
2024-05-14 21:05:02 +0200

    fix codice + aggiunta input porta

--- static/js/profile_actions.js
+++ static/js/profile_actions.js
@@ -40,10 +40,12 @@ function checkInput(input) {
         errorMsg.innerHTML = "";
         html_block = '<i class="fa-solid fa-xmark"></i>';
         confirm.disabled = true;
+        input1 = false;
     } else {
         errorMsg.innerHTML = "ID non valido: deve contenere 9 numeri";
         html_block = '<i class="fa-solid fa-xmark"></i>';
         confirm.disabled = true;
+        input1 = false;
     }
     $("#confirmIDcheck").append(html_block)
 }
@@ -55,7 +57,7 @@ function checkGroupInput(input) {
     $("#confirmGroupIDcheck").empty();
     html_block = "";
     // Verifica se l'input contiene solo numeri e ha una lunghezza compresa tra 1 e 9
-    if (/^\d{14}$/.test(tgid)) {
+    if (/^\d{13}$/.test(tgid)) {
         errorMsg.innerHTML = "";
         html_block = '<i class="fa-solid fa-check"></i>';
         if (input1 == true) {
@@ -66,15 +68,20 @@ function checkGroupInput(input) {
         errorMsg.innerHTML = "";
         html_block = '<i class="fa-solid fa-xmark"></i>';
         confirm.disabled = true;
+        input2 = false;
     } else {
-        errorMsg.innerHTML = "ID non valido: deve contenere 14 numeri";
+        errorMsg.innerHTML = "ID non valido: deve contenere 13 numeri";
         html_block = '<i class="fa-solid fa-xmark"></i>';
         confirm.disabled = true;
+        input2 = false;
     }
     $("#confirmGroupIDcheck").append(html_block)
 }
 
 //check if IP is right
+
+var ipCheck = false, portCheck = false;
+
 function checkInputIP(input) {
     var IPaddress = input.value.trim();
     var doBackup = document.getElementById("backupBtn");
@@ -86,16 +93,54 @@ function checkInputIP(input) {
     // Check if the input is a valid IP address
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(IPaddress)) {
         error.innerHTML = "";
-        doBackup.disabled = false; // Enable doBackup
-        html_block = '<i class="fa-solid fa-check"></i>';
+        ipCheck = true;
+        html_block = '<i class="fa-solid fa-xmark"></i>';
+        if (portCheck == true) {
+            doBackup.disabled = false; // Enable doBackup
+            html_block = '<i class="fa-solid fa-check"></i>';
+        }
     } else if (IPaddress == '') {
         error.innerHTML = "";
         doBackup.disabled = true; // Disable doBackup
         html_block = '<i class="fa-solid fa-xmark"></i>';
+        ipCheck = false;
     } else {
         error.innerHTML = "L'indirizzo IP inserito non &egrave valido";
         doBackup.disabled = true; // Disable doBackup
         html_block = '<i class="fa-solid fa-xmark"></i>';
+        ipCheck = false;
+    }
+    $("#confirmIPcheck").append(html_block)
+}
+
+//check if port is right
+function checkInputPort(input) {
+    var port = input.value.trim();
+    var doBackup = document.getElementById("backupBtn");
+    var error = document.getElementById("IPerror");
+
+    $("#confirmIPcheck").empty();
+    html_block = "";
+
+    // Check if the input is a valid IP address
+    if (/^\d+$/.test(port)) {
+        error.innerHTML = "";
+        portCheck = true;
+        html_block = '<i class="fa-solid fa-xmark"></i>';
+        if (ipCheck == true) {
+            doBackup.disabled = false; // Enable doBackup
+            html_block = '<i class="fa-solid fa-check"></i>';
+        }
+    } else if (port == "") {
+        error.innerHTML = "";
+        doBackup.disabled = true; // Disable doBackup
+        html_block = '<i class="fa-solid fa-xmark"></i>';
+        portCheck = false;
+    } else {
+        error.innerHTML = "La porta inserita non &egrave valida";
+        doBackup.disabled = true; // Disable doBackup
+        html_block = '<i class="fa-solid fa-xmark"></i>';
+        portCheck = false;
     }
     $("#confirmIPcheck").append(html_block)
 }
