2be19ff5bc4301632b3f342bbb0a7d22e0a10d1e
FilippoTosin <97382927+FilippoTosin@users.noreply.github.com>
2024-05-16 23:47:31 +0200

    Add of confetti effects and some adjustments

--- snake.js
+++ snake.js
@@ -1,282 +1,270 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-const blockSize = 20;
-let snake = [{ x: 10, y: 10 }];
-let food = { x: 15, y: 10 };
-let dx = 1;
-let dy = 0;
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 15, snakeY = 15;           // To spawn the snake in the centre of the field
+let speedX = 0, speedY = 0;
+let snakeBody = [];
+let setIntervalId;
 let score = 0;
-let isGameOver = false;
-let gameLoop; 
+let touchSupport = 0;
+var initialX = null, initialY = null;
 
-function drawBlock(x, y) {
-  ctx.fillStyle = "green";
-  ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+
+function restartGame(){
+    gameOver = false;
+    foodX = 0, foodY = 0;
+    snakeX = 15, snakeY = 15;
+    speedX = 0, speedY = 0;
+    snakeBody = [];
+    score = 0;
+    clearInterval(setIntervalId);
+    document.getElementById("gameOverScreen").style.display = "none";
+    scoreElement.innerText = "Punteggio: 0";
+    foodPosition();
+
+    setIntervalId = setInterval(initGame, 100);         // Start the game
 }
 
-function drawSnake() {
-  snake.forEach(segment => drawBlock(segment.x, segment.y));
+
+// Section of controls to see if the device is touchscreen or not, this is useful to know if the movements should be taken by touch or keyboard arrows
+
+// Check if the device support multiple touch, if it support it, it could be a touchscreen device
+function hasTouchSupport(){
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 }
 
-function drawFood() {
-  drawBlock(food.x, food.y);
+
+// Check if the device supports touch events using a combination of 'ontouchstart' in window and the DocumentTouch interface
+function isTouchDevice(){
+    return 'ontouchstart' in window || (window.DocumentTouch && document instanceof window.DocumentTouch);
 }
 
-function moveSnake() {
-  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-  snake.unshift(head);
-  if (head.x === food.x && head.y === food.y) {
-    score += 10;
-    document.getElementById("score").innerText = score;
-    generateFood();
-  } else {
-    snake.pop();
-  }
+
+// Check based on the width of the device
+function isMobile(){
+    const minWidth = 768;           // Minimum width for desktop devices
+    return window.innerWidth < minWidth || screen.width < minWidth;
 }
 
-function generateFood() {
-  food.x = Math.floor(Math.random() * (canvas.width / blockSize));
-  food.y = Math.floor(Math.random() * (canvas.height / blockSize));
-  snake.forEach(segment => {
-    if (food.x === segment.x && food.y === segment.y) {
-      generateFood();
+// Function that groups the checks and their calls
+function touchSupportCheck(){
+
+    if(hasTouchSupport()){
+        touchSupport+=1;
     }
-  });
+    if(isTouchDevice()){
+        touchSupport+=1;
+    }
+    if(isMobile()){
+        touchSupport+=1;
+    }
+    return touchSupport;
 }
 
 
-function loadAndDisplayScore() {
-  var savedScore = localStorage.getItem('savedScore');
-  if (!savedScore || score > parseInt(savedScore)) {
-    // Aggiorniamo lo score salvato
-    localStorage.setItem('savedScore', score);
-  }
-  // Recuperiamo lo score salvato dal localStorage
-  var savedScore = localStorage.getItem('savedScore');
-  // Verifichiamo se lo score è stato salvato precedentemente
-  if (savedScore) {
-    // Aggiorniamo l'elemento h1 sulla pagina di gioco con lo score salvato
-    document.getElementById("bscore").innerText =savedScore;
-  }
+// Getting high score from the local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `Record personale: ${highScore}`;
+
+// Assign a random value from 1 to 30 to X and Y to determine a position
+function foodPosition(){
+    foodX = Math.floor(Math.random()*30)+1;
+    foodY = Math.floor(Math.random()*30)+1;
 }
 
 
+function handleGameOver(){
+    // Resetting the timer and reloading the page on game over
+    clearInterval(setIntervalId);
+    document.getElementById("gameOverScreen").style.display = "block";
+    document.getElementById("scoreDisplay").innerText = "Punteggio ottenuto: " + score;
 
-function loadAndDisplayBestScores() {
-  // Recuperiamo i migliori punteggi salvati dal localStorage (supponiamo che sia una lista di oggetti {player: "Nome", score: valore})
+    if(score > highScore){
+        highScore = score;
+        localStorage.setItem("high-score", highScore);
+        highScoreElement.innerText = `Record personale: ${highScore}`;
+    }
 
-  var table = document.getElementById("table");
+    // Event listener for the button "Continua"
+    function handleContinueButtonClick() {
+        document.getElementById("gameOverScreen").style.display = "none";
+        foodPosition();
+        restartGame();
+    }
 
-  // Array per contenere i valori della seconda colonna
-  var values = [];
+    document.getElementById("continueButton").addEventListener("click", handleContinueButtonClick);
+}
 
-  // Iteriamo sulle righe della tabella (escludendo l'intestazione)
-  for (var i = 1; i < table.rows.length; i++) {
-    // Accediamo alle celle nella prima e nella seconda colonna di ciascuna riga
-    var playerNameCell = table.rows[i].cells[0];
-    var scoreCell = table.rows[i].cells[1];
+
+function changeDirectionK(e){       // Function to change the direction of the snake based on the keyboard input
+    // Check if the key pressed is the "ArrowUp" key, the snake is not currently moving downward,
+    // and either the snake's body length is 1 or less, or if it is greater than 1,
+    // ensure that the current vertical speed is not moving upward.
+    if(e.key === "ArrowUp" && speedY !== 1 && (snakeBody.length <= 1 || speedY !== -1)){
+        speedX = 0;
+        speedY = -1;
+    }
+    else if(e.key === "ArrowDown" && speedY != -1){
+        speedX = 0;
+        speedY = 1;
+    }
+    else if(e.key === "ArrowLeft" && speedX != 1){
+        speedX = -1;
+        speedY = 0;
+    }
+    else if(e.key === "ArrowRight" && speedX != -1){
+        speedX = 1;
+        speedY = 0;
+    }
+}
+
+
+function startTouch(e){
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+};
+
+
+function moveTouch(e){
+
+    if(initialX === null){
+        return;
+    }
+
+    if(initialY === null){
+        return;
+    }
+ 
+    var currentX = e.touches[0].clientX;            // Obtain the current X coordinate of the touch relative to the top-left corner of the touched element
+    var currentY = e.touches[0].clientY;
     
-    // Aggiungiamo il nome del giocatore e il punteggio all'array
-    var playerName = playerNameCell.textContent || playerNameCell.innerText;
-    var score = parseInt(scoreCell.textContent || scoreCell.innerText);
-    values.push({ player: playerName, score: score });
-  }
+    var diffX = initialX - currentX;            // Calculate the difference between the initial X and Y coordinate and the current X and Y coordinate of the touch for understanding the direction of the swipe
+    var diffY = initialY - currentY;
 
-  var newPlayerName = document.getElementById("NamePlayer").textContent;
-  var newScore = parseInt(document.getElementById("bscore").textContent);
-  new_table={ player: newPlayerName, score: newScore };
-  tmp=containsValueInArrayOfObjects(values, 'player', newPlayerName)
-  if(tmp==false){
-    values.push(new_table)
-    console.log("fds".values);
-  }else{
-    if(values[tmp][score]<newScore){
-      values[tmp][score]=newScore
-      sortByKey(values, 'score');
-      console.log("das".values);
-    }else{
-      console.log("ok".values);
-      return;
-      
+    if(Math.abs(diffX) > Math.abs(diffY)){
+
+        // Horizontal swipe
+        // Ensure that the current horizontal speed is not moving right (speedX !== 1)
+        // Allow changing direction if the snake's body length is exactly 1 or if it is greater than 1, ensure that the current horizontal speed is not moving left (speedX !== -1)
+        if(diffX > 0 && speedX !== 1 && (snakeBody.length == 1 || speedX !== -1)){
+            // Swiped left
+            speedX = -1;
+            speedY = 0;
+        }
+        else if(diffX < 0 && speedX !== -1 && (snakeBody.length == 1 || speedX !== 1)){
+            // Swiped right
+            speedX = 1;
+            speedY = 0;
+        }
     }
-  } 
-   
-  console.log(values);
-  updateLeaderboardHTML(values);
-}
-
-function updateLeaderboardHTML(values) {
-  var tbody = document.querySelector('#table tbody');
-  tbody.innerHTML = ''; // Pulisce il contenuto attuale della tabella
-
-  // Itera attraverso l'array di punteggi e crea una nuova riga per ogni elemento
-  values.forEach(function(entry) {
-      var row = document.createElement('tr');
-      var playerNameCell = document.createElement('td');
-      playerNameCell.textContent = entry.player;
-      var scoreCell = document.createElement('td');
-      scoreCell.textContent = entry.score;
-
-      row.appendChild(playerNameCell);
-      row.appendChild(scoreCell);
-      tbody.appendChild(row);
-  });
-}
-
-
-function sortByKey(arr, key) {
-  arr.sort(function(a, b) {
-      var valueA = a[key];
-      var valueB = b[key];
-      if (valueA < valueB) {
-          return -1;
-      }
-      if (valueA > valueB) {
-          return 1;
-      }
-      return 0;
-  });
-}
-
-function containsValueInArrayOfObjects(arr, key, value) {
-  for (var i = 0; i < arr.length; i++) {
-      if (arr[i][key] === value) {
-          return i;
-      }
-  }
-  return false;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function saveScoreAndPlayerToBestScores(player, score) {
-  // Recuperiamo i migliori punteggi salvati dal localStorage
-  var bestScores = JSON.parse(localStorage.getItem('bestScores')) || [];
-  // Aggiungiamo il nuovo punteggio e il giocatore alla lista dei migliori punteggi
-  bestScores.push({ player: player, score: score });
-  // Salviamo la lista aggiornata dei migliori punteggi nel localStorage
-  localStorage.setItem('bestScores', JSON.stringify(bestScores));
-}
-
-
-function checkCollision() {
-  const head = snake[0];
-  if (head.x < 0 || head.x >= canvas.width / blockSize || head.y < 0 || head.y >= canvas.height / blockSize) {
-    gameOver();
-  }
-  for (let i = 1; i < snake.length; i++) {
-    if (head.x === snake[i].x && head.y === snake[i].y) {
-      gameOver();
+    else{   // Vertical swipe
+        if(diffY > 0 && speedY !== 1 && (snakeBody.length == 1 || speedY !== -1)){
+            // Swiped up
+            speedX = 0;
+            speedY = -1;
+        }
+        else if(diffY < 0 && speedY !== -1 && (snakeBody.length == 1 || speedY !== 1)){
+            // Swiped down
+            speedX = 0;
+            speedY = 1;
+        }
     }
-  }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+};
+
+
+function changeDirectionT(e){
+    if (e.type === "touchstart" || e.type === "touchmove") {
+        moveTouch(e);
+    }
 }
 
-function gameOver() {
-  isGameOver = true;
-  clearInterval(gameLoop);
-  document.getElementById("gameOverText").innerText = "Game Over! Your score: " + score;
-  document.getElementById("score").innerText = "0";
 
-  loadAndDisplayScore();
-  loadAndDisplayBestScores();
-
-}
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function updateGame() {
-  if (isGameOver) return;
-  clearCanvas();
-  drawSnake();
-  drawFood();
-  moveSnake();
-  checkCollision();
-  
-}
-
-function keyDownHandler(event) {
-  const keyPressed = event.key;
-  if (keyPressed === "ArrowUp" && dy === 0) {
-    dx = 0;
-    dy = -1;
-  } else if (keyPressed === "ArrowDown" && dy === 0) {
-    dx = 0;
-    dy = 1;
-  } else if (keyPressed === "ArrowLeft" && dx === 0) {
-    dx = -1;
-    dy = 0;
-  } else if (keyPressed === "ArrowRight" && dx === 0) {
-    dx = 1;
-    dy = 0;
-  }
-}
-// Funzione per gestire gli eventi touch su un elemento
-function handleTouch(elementId, callback) {
-  const element = document.getElementById(elementId);
-  element.addEventListener('touchstart', function(event) {
-    event.preventDefault(); // Evita il comportamento predefinito del touch (es. lo zoom)
-    callback();
-  });
-}
-
-// Gestione degli eventi touch per i controlli del serpente
-handleTouch('up-touch', function() {
-  if (dy !== 1) { // Assicura che non stia andando verso il basso
-    dx = 0;
-    dy = -1;
-  }
-});
-
-handleTouch('left-touch', function() {
-  if (dx !== 1) { // Assicura che non stia andando verso destra
-    dx = -1;
-    dy = 0;
-  }
-});
-
-handleTouch('right-touch', function() {
-  if (dx !== -1) { // Assicura che non stia andando verso sinistra
-    dx = 1;
-    dy = 0;
-  }
-});
-
-handleTouch('down-touch', function() {
-  if (dy !== -1) { // Assicura che non stia andando verso l'alto
-    dx = 0;
-    dy = 1;
-  }
+// Call to "changeDirectionK" on each key click and passing key dataset value as an object
+controls.forEach(function(button) {
+    button.addEventListener("click", function() {
+        changeDirectionK({ key: button.dataset.key });
+    });
 });
 
 
-function startGame() {
-  snake = [{ x: 10, y: 10 }];
-  dx = 1;
-  dy = 0;
-  score = 0;
-  isGameOver = false;
-  document.getElementById("gameOverText").innerText = "";
-  document.getElementById("score").innerText = score;
-  generateFood();
-  clearInterval(gameLoop);
-  gameLoop = setInterval(updateGame, 100);
-  
+function launchConfetti(){
+
+    confetti({
+        particleCount: 100,         // Set the number of confetti
+        angle: -25,         // Set the angle that confetti will be thrown
+        spread: 70,         // Set the spread of the thrown
+        origin: {x:0.1, y:0.1}          // Set the point from which the confetti will be thrown
+    });
+
+    confetti({
+        particleCount: 100,
+        angle: 205,
+        spread: 70,
+        origin: {x:0.9, y:0.1}
+    });
 }
 
-document.addEventListener("keydown", keyDownHandler);
 
+function initGame(){
+    if(gameOver)
+        return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY}/${foodX}"></div>`;
+
+    // Check if the snake hit the food
+    if(snakeX == foodX && snakeY == foodY){
+        foodPosition();
+        snakeBody.push([foodY, foodX]);         // Pushing food position to snake body array
+        score++;
+        highScore = Math.max(score, highScore);
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Punteggio: ${score}`;
+        if(score === 50){
+            launchConfetti();
+        }
+    }
+
+    // Updating the snake's head position based on the current speed
+    snakeX += speedX;
+    snakeY += speedY;
+    
+    for(let i=snakeBody.length-1; i>0; i--){            // Shifting forward the values of the elements in the snake body by one
+        snakeBody[i] = snakeBody[i-1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];            // Setting first element of snake body to current snake position
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){           // Checking if the snake's head is out of wall, if so setting gameOver to true
+        return gameOver = true;
+    }
+
+    for(let i=0; i<snakeBody.length; i++){
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;          // Adding a div for each part of the snake's body
+        
+        if(i !== 0 && snakeBody[0][1] == snakeBody[i][1] && snakeBody[0][0] == snakeBody[i][0]){            // Check if the snake head hit the body, in this case it set gameOver to true
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+
+}
+
+document.getElementById("gameOverScreen").style.display = "none";           // Hide the "Game Over" screen
+foodPosition();
+setIntervalId = setInterval(initGame, 100);
+
+if(touchSupportCheck() >= 2){           // If touchSupport is 2 or 3 means that probably is a touchscreen device so the EventListener is added
+    document.addEventListener("touchstart", startTouch, false);
+    document.addEventListener("touchmove", changeDirectionT, false);
+}
+else{
+    document.addEventListener("keyup", changeDirectionK);
+}
