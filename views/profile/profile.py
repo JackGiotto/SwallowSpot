1db72e258a9d6ee595b7ebcbd672e2b08946b8c7
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-05-16 23:59:02 +0200

    fix profile page

--- views/profile/profile.py
+++ views/profile/profile.py
@@ -16,9 +16,8 @@ def user():
         id_user = id_user[0]["ID_user"]
         id_role = db.executeQuery("SELECT ID_role FROM User WHERE username ='"+session["username"]+"';")
         id_role = id_role[0]["ID_role"]
-
+        show_button = id_role>1
         if request.method == "GET":
-            show_button = id_role>1
             return render_template("user/profile.html",  username=session["username"], user_perms=id_role, show_button=show_button)
 
 
@@ -27,22 +26,23 @@ def user():
                 new_username = request.form["new_username"]
                 result = db.executeQuery("SELECT username FROM User where username = '" + new_username+ "';")
                 # check if username is already taken
+                
                 if bool(result):
-                    return render_template("user/profile.html", msg="username non disponibile", username=session["username"])
+                    return render_template("user/profile.html", msg="username non disponibile", username=session["username"], show_button=show_button)
                 else:
                     db.executeQuery("UPDATE User SET username = '"+str(new_username)+"' WHERE ID_user = '"+str(id_user)+"';")
                     session["username"] = new_username
-                    return render_template("user/profile.html", username=session["username"])
+                    return render_template("user/profile.html", username=session["username"], show_button=show_button)
 
             elif "city" in request.form: # city change
                 new_zone = request.form["city"]
                 if new_zone not in get_cities(want_list=True):
-                    return render_template("user/profile.html", username=session["username"], msg="Errore: Il comune inserito non è valido")
+                    return render_template("user/profile.html", username=session["username"], msg_area="Errore: Il comune inserito non è valido", show_button=show_button)
                 
                 new_zone = db.executeQuery("SELECT Topology.ID_area FROM Topology WHERE Topology.city_name = '"+str(new_zone)+"';")
                 new_zone= new_zone[0]["ID_area"]
                 db.executeQuery("UPDATE User SET ID_area = "+str(new_zone)+" WHERE ID_user = "+str(id_user)+";")
-                return render_template("user/profile.html", username=session['username'])
+                return render_template("user/profile.html", username=session['username'], show_button=show_button)
 
             elif "new_password" in request.form: # password change
                 new_password = request.form["new_password"]
@@ -53,13 +53,13 @@ def user():
                 old_password=hash_password(old_password)
                 result=db.executeQuery("SELECT User.password FROM User WHERE password = '"+str(old_password)+"';")
                 if not result:
-                    return render_template("user/profile.html", msg2="Errore: La password vecchia non è corretta ", username=session["username"])
+                    return render_template("user/profile.html", msg2="Errore: La password vecchia non è corretta ", username=session["username"], show_button=show_button)
                 if (len(new_password) < 8 or not has_number(new_password) or not has_uppercase(new_password) or not has_special_character(new_password)):
-                    return render_template("user/profile.html", msg2="Errore: La password deve contenere almeno 8 caratteri, un numero, una maiuscola e un carattere speciale", username=session["username"])
+                    return render_template("user/profile.html", msg2="Errore: La password deve contenere almeno 8 caratteri, un numero, una maiuscola e un carattere speciale", username=session["username"], show_button=show_button)
                 # hasing password
                 new_password=hash_password(new_password)
                 db.executeQuery("UPDATE User SET password = '"+str(new_password)+"' WHERE ID_user = '"+str(id_user)+"';")
-                return render_template("user/profile.html", username=session['username'])
+                return render_template("user/profile.html", username=session['username'], show_button=show_button)
             
             elif "passwordDelete" in request.form: # delete account
                 password = request.form['passwordDelete']
@@ -73,9 +73,9 @@ def user():
                     db.executeQuery("DELETE FROM `User` WHERE `username` = '"+str(username)+"'")
                     return redirect(url_for("home.home"))
                 else:
-                    return render_template("user/profile.html", msgpsw="Errore: password non corretta", username = session["username"])
+                    return render_template("user/profile.html", msgpsw="Errore: password non corretta", username = session["username"], show_button=show_button)
 
-        return render_template("user/profile.html")
+        return render_template("user/profile.html", show_button=show_button)
     else:
         return render_template("user/settings.html")
 
