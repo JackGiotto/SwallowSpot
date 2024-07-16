55bf0c6029475e9b7b042e3d56115614ea40a502
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-07-16 15:54:51 +0200

    fix backend feedbacks user side #2

--- views/feedbacks/feedbacks.py
+++ views/feedbacks/feedbacks.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, redirect, session, url_for
+from flask import Blueprint, render_template, session, request
 from models import db
-from utils.risks import convert_risk_color, get_query_last_hydro, parse_date_us_it
 import json
-from Datetime import Datetime as dt
+from datetime import datetime as dt
 
 feedbacks_bp = Blueprint('feedbacks', __name__, template_folder='templates')
 
@@ -11,16 +10,22 @@ def feedbacks():#charge the page
     if(request.method == 'GET'):
         return render_template("feedbacks.html")
     elif(request.method == 'POST'):
-        #check if the user in sessioned and return his own role
+        # check if the user in sessioned and return his own role
         if "username" in session:
             role = db.executeQuery("SELECT ID_role FROM User WHERE username ='"+session["username"]+"';")
             role = role[0]["ID_role"]
-        else:#else judge him like a normal user
+        else: # else judge him like a normal user
             role = 1
-        if "feedback" in request.form:
-            #fetching the information's feedback
-            object = request.form["object"]
-            description = request.form["message"]
-            #execution of the query on database
-            db.executeQuery("INSERT INTO Feedback (object, description, date, validate, ID_role) VALUES('title of the feedback ("+object+")', "+description+", NOW(), false, "+role+");")
-
+        if "feedObject" in request.form:
+            # fetching the information's feedback
+            feedObject = request.form["feedObject"].replace("'", "")
+            feedDesc = request.form["feedDesc"].replace("'", "")
+            # execution of the query on database
+            query = f'''
+                        INSERT INTO feedback (object, description, date, validate, ID_role)
+                        VALUES(
+                        '{feedObject}', '{feedDesc}', NOW(), 0, {role}
+                        );
+                    '''
+            db.executeQuery(query)
+            return render_template("feedbacks.html", feedback_success = "Feedback inviato con successo")
