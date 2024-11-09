6e84c4d631e73c2e80d9416eaa4eafc312ceb914
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-11-09 22:44:44 +0100

    changed absolute path to the one used in server

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     load_dotenv()
     start_path = "./"
 else:
-    start_path = "/home/maggiottobackend/SwallowSpot/"
+    start_path = "/var/www/SwallowSpot/"
     read_env_file(start_path + ".env")
 
 os.environ["start_path"] = start_path
--- swallowspot.wsgi.py
+++ swallowspot.wsgi.py
@@ -2,12 +2,12 @@ import sys
 import os
 
 # project directory
-project_home = '/home/maggiottobackend/SwallowSpot'
+project_home = '/var/www/SwallowSpot'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
 # Virtual enviornement
-sys.path.insert(0,"/home/maggiottobackend/SwallowSpot/venv/lib/python3.11/site-packages")
+sys.path.insert(0,"/var/www/SwallowSpot/venv/lib/python3.11/site-packages")
 
 # Import your Flask app
 from app import app as application
