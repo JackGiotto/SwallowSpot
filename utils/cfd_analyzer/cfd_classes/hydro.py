88d8a80a214cd694223ada41158a211a511138cc
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-11-12 09:38:08 +0100

    fixed #10

--- utils/cfd_analyzer/cfd_classes/hydro.py
+++ utils/cfd_analyzer/cfd_classes/hydro.py
@@ -2,11 +2,11 @@ import camelot
 import json
 from utils.get_data import convert_date
 from models import db
-import PyPDF2
 import os
+import base64
 
 class Hydro:
-	
+
 
 	# the position in the table of the date
 	DATE_POSITION = {
@@ -25,7 +25,7 @@ class Hydro:
 		self.PAGES_NUMBERS["risk"] = pages
 		self.table_number = table_number
 		self._get_bulletin_data()
-		
+
 	def get_data(self) -> dict:
 		"""get date and risks of the bulletin
 		"""
@@ -40,7 +40,7 @@ class Hydro:
 		report_query = queries["bulletin_query"]
 		first_query = [report_query, last_index]
 		report_id = db.executeTransaction(first_query, select=True)["new_id"]
-		
+
 		# debug
 		print("Report ID:", report_id)
 
@@ -59,15 +59,21 @@ class Hydro:
 		#print(self.data["risks"])
 		queries = {"bulletin_query": "", "risks_queries": []}
 		path = self.path
+		with open(path, "rb") as f:
+			pdf_data = base64.b64encode(f.read()).decode('utf-8')
+			# pdf_data = f.read()
 
+		"""
 		if (os.getenv("start_path") != "./"):
 			print("different")
 			path = path.replace(os.getenv("start_path"), "./")
+		"""
 
 		queries["bulletin_query"] = f'''
-					INSERT INTO Report(starting_date, ending_date, path) VALUES
-					("{self.data["date"]["starting_date"]}", "{self.data["date"]["ending_date"]}", "{path}");
+					INSERT INTO Report(starting_date, ending_date, pdf_data) VALUES
+					("{self.data["date"]["starting_date"]}", "{self.data["date"]["ending_date"]}", "{pdf_data}");
 				'''
+		print(queries["bulletin_query"])
 		for key, values in self.data["risks"].items():
 			area_name = key
 			risk_name = ""
@@ -80,13 +86,14 @@ class Hydro:
 						f'''SET @ID_color := (SELECT ID_color FROM Color WHERE color_name = '{color_name}');''',
 						f'''INSERT INTO Criticalness(ID_area, ID_risk, ID_color, ID_report) VALUES (@ID_area, @ID_risk, @ID_color, @ID_report);''']
 				queries["risks_queries"].append(query)
+		#print(queries)
 		return queries
 
-	def _get_bulletin_data(self):		
+	def _get_bulletin_data(self):
 		print("Analyzing Hydro bulletin, path:", self.path)
 
 		self.data["date"] = self._get_date(camelot.read_pdf(self.path, flavor='stream', pages=self.PAGES_NUMBERS["date"])[self.table_number].df)
-		self.data["risks"] = self._get_risks(camelot.read_pdf(self.path, flavor='stream', pages=self.PAGES_NUMBERS["risk"])[self.table_number].df) 
+		self.data["risks"] = self._get_risks(camelot.read_pdf(self.path, flavor='stream', pages=self.PAGES_NUMBERS["risk"])[self.table_number].df)
 		print("Finished analysis\n", json.dumps(self.data, indent="\t"))
 
 	def _get_date(self, table) -> dict[str, str]:
@@ -98,16 +105,16 @@ class Hydro:
 
 		starting_date = convert_date(splitted[2])
 		starting_date += " " + splitted[4] + ":00" # add seconds to starting date
-		
+
 		ending_date = convert_date(splitted[7])
 		ending_date += " " + splitted[9]+ ":00"  # add seconds to ending date
 		return {"starting_date": starting_date, "ending_date":ending_date}
-		
+
 
 	def _get_risks(self, table) -> dict[str, any]:
 		"""get the value associated with every risk
 		"""
-		
+
 		# risks template for hydro
 		with open(os.getenv("start_path") + "utils/cfd_analyzer/templates/risks_template_hydro.json", "r") as f:
 			RISKS = json.load(f)
@@ -120,7 +127,7 @@ class Hydro:
 
 			# delete unnecessary information
 			del region_data["row"]
-		
+
 		# debug
 		#print(template)
 
