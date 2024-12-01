6f210373b9dd16e91db83290cf1b6881efd1254a
JackGiotto <107201625+JackGiotto@users.noreply.github.com>
2024-12-01 15:27:08 +0100

    Fixed admin infos showing also to not admins #13

--- views/info/info.py
+++ views/info/info.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, redirect, session, url_for
-import json
+from models import db
 
 info_bp = Blueprint('info', __name__, template_folder='templates')
 
 @info_bp.route('/info/', methods=['GET'])
 def info():
-    return render_template("info.html")
+    if "username" in session:
+        id_role = db.executeQuery("SELECT ID_role FROM User WHERE username ='"+session["username"]+"';")
+        id_role = id_role[0]["ID_role"]
+        show_admin = id_role > 1
+    else:
+        show_admin = False
+    return render_template("info.html", show_admin=show_admin)
